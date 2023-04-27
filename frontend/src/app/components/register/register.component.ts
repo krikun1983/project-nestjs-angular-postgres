183859931a71f8841bae097858d1510caf55aa13
Stanislav Akhmadeev <stanislav.akhmadeev@idp.zyfra.com>
2023-04-27 21:59:30 +0300

    feat: added custom-validators with reactive form for registration form

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs';
@@ -11,6 +18,22 @@ type RegisterForm = {
   password: FormControl<string>;
   passwordConfirm: FormControl<string>;
 };
+
+class CustomValidators {
+  static passwordContainsNumber(control: AbstractControl): ValidationErrors | null {
+    const regex = /\d/;
+    return regex.test(control.value) && control.value !== null ? null : { passwordInvalid: true };
+  }
+
+  static passwordMatch(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const passwordConfirm = control.get('passwordConfirm')?.value;
+
+    return password === passwordConfirm && !!password && !!passwordConfirm
+      ? null
+      : { passwordsNoMatching: true };
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -27,28 +50,33 @@ export class RegisterComponent implements OnInit {
   }
 
   private createForm(): void {
-    this.registerForm = this.fb.group({
-      name: this.fb.control('', {
-        nonNullable: true,
-        validators: [Validators.required],
-      }),
-      username: this.fb.control('', {
-        nonNullable: true,
-        validators: [Validators.required],
-      }),
-      email: this.fb.control('', {
-        nonNullable: true,
-        validators: [Validators.required, Validators.email, Validators.minLength(6)],
-      }),
-      password: this.fb.control('', {
-        nonNullable: true,
-        validators: [Validators.required, Validators.minLength(3)],
-      }),
-      passwordConfirm: this.fb.control('', {
-        nonNullable: true,
-        validators: [Validators.required],
-      }),
-    });
+    this.registerForm = this.fb.group(
+      {
+        name: this.fb.control('', {
+          nonNullable: true,
+          validators: [Validators.required],
+        }),
+        username: this.fb.control('', {
+          nonNullable: true,
+          validators: [Validators.required],
+        }),
+        email: this.fb.control('', {
+          nonNullable: true,
+          validators: [Validators.required, Validators.email, Validators.minLength(6)],
+        }),
+        password: this.fb.control('', {
+          nonNullable: true,
+          validators: [Validators.required, Validators.minLength(3), CustomValidators.passwordContainsNumber],
+        }),
+        passwordConfirm: this.fb.control('', {
+          nonNullable: true,
+          validators: [Validators.required],
+        }),
+      },
+      {
+        validators: CustomValidators.passwordMatch,
+      }
+    );
   }
 
   onSubmit(): void {
